6e3945018625ec5ac18ae9ad7799b28b03b3f615
zobayerdev <zobayer.dev@gmail.com>
2023-04-23 23:46:13 +0600

    text to image code complete -nayem

--- app/src/main/java/com/trodev/visitingcardmaker/MainActivity.java
+++ app/src/main/java/com/trodev/visitingcardmaker/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,7 +17,30 @@ import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.Toast;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,10 +54,26 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private long pressedTime;
 
+    private LinearLayout infoLl;
+    private Button saveBtn;
+    private static final String TAG = "SAVE_BITMAP";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoLl = findViewById(R.id.infoLl);
+        saveBtn = findViewById(R.id.saveBtn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = getBitmapFromUiView(infoLl);
+                saveBitmapImage(bitmap);
+            }
+        });
 
         getSupportActionBar().setTitle("Dashboard");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -153,5 +193,88 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
         }
         pressedTime = System.currentTimeMillis();
+    }
+
+    private Bitmap getBitmapFromUiView(View view) {
+
+        //Define a bitmap with the same size as the view
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Bind a canvas to it
+        Canvas canvas = new Canvas(returnedBitmap);
+
+        //Get the view's background
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas);
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE);
+        }
+        // draw the view on the canvas
+        view.draw(canvas);
+
+        //return the bitmap
+        return returnedBitmap;
+    }
+
+    /// @param folderName can be your app's name
+    private void saveBitmapImage(Bitmap bitmap) {
+        long timestamp = System.currentTimeMillis();
+
+        //Tell the media scanner about the new file so that it is immediately available to the user.
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+        values.put(MediaStore.Images.Media.DATE_ADDED, timestamp);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, timestamp);
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + getString(R.string.app_name));
+            values.put(MediaStore.Images.Media.IS_PENDING, true);
+            Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            if (uri != null) {
+                try {
+                    OutputStream outputStream = getContentResolver().openOutputStream(uri);
+                    if (outputStream != null) {
+                        try {
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                            outputStream.close();
+                        } catch (Exception e) {
+                            Log.e(TAG, "saveToGallery: ", e);
+                        }
+                    }
+                    values.put(MediaStore.Images.Media.IS_PENDING, false);
+                    getContentResolver().update(uri, values, null, null);
+
+                    Toast.makeText(this, "Saved...", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Log.e(TAG, "saveToGallery: ", e);
+                }
+            }
+        } else {
+            File imageFileFolder = new File(Environment.getExternalStorageDirectory().toString() + '/' + getString(R.string.app_name));
+            if (!imageFileFolder.exists()) {
+                imageFileFolder.mkdirs();
+            }
+            String mImageName = "" + timestamp + ".png";
+
+            File imageFile = new File(imageFileFolder, mImageName);
+            try {
+                OutputStream outputStream = new FileOutputStream(imageFile);
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                    outputStream.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "saveToGallery: ", e);
+                }
+                values.put(MediaStore.Images.Media.DATA, imageFile.getAbsolutePath());
+                getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                Toast.makeText(this, "Saved...", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Log.e(TAG, "saveToGallery: ", e);
+            }
+        }
     }
 }
